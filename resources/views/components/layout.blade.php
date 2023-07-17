828de9c7f35e5fa78ee1015e0af42359727d26fd
fabio-adaniya <f1hiroki@outlook.com>
2023-07-17 18:07:53 -0300

    alteração no layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,36 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg bg-dark bg-opacity-10">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('chamados.create') }}" class="nav-link">Criar chamado</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('chamados.index') }}" class="nav-link">Consultar chamados</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Configurações</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
         {{ $slot }}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
