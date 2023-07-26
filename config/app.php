ccd184bc4eee5119aa79ec4aef505adf5892f9bb
fabio-adaniya <f1hiroki@outlook.com>
2023-07-26 17:42:09 -0300

    possibilitando acesso aos models no blade

--- config/app.php
+++ config/app.php
@@ -226,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Status' => App\Models\Status::class,
+        'Urgencia' => App\Models\Urgencia::class,
     ],
-
 ];
