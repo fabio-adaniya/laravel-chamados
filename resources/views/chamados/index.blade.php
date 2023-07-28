58dc617e2db7c38d96d8d2f4d430df8908de7b13
fabio-adaniya <f1hiroki@outlook.com>
2023-07-28 17:26:30 -0300

    melhorias na tela de consulta do chamado

--- resources/views/chamados/index.blade.php
+++ resources/views/chamados/index.blade.php
@@ -13,7 +13,9 @@
                                 Buscar apenas os meus chamados
                             </label>
                         </div>
-                        <button type="submit" class="btn btn-sm btn-outline-success ms-3">Pesquisar</button>
+                        <button type="submit" class="btn btn-sm btn-outline-success ms-3">
+                            <i class="fa-solid fa-magnifying-glass"></i> Pesquisar
+                        </button>
                     </div>
                 </form>
             </div>
@@ -21,33 +23,35 @@
     @endif
 
     <div class="card m-3">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Urgência</th>
-                    <th scope="col">Abertura</th>
-                    <th scope="col">Última atualização</th>
-                    <th scope="col">Título</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse($chamados as $chamado)
+        <div class="table-responsive">
+            <table class="table table-sm">
+                <thead>
                     <tr>
-                        <td style="width: 1%">{{ $chamado->id }}</td>
-                        <td style="width: 1%">{{ Status::DESCRICAO[$chamado->status] }}</td>
-                        <td style="width: 1%">{{ Urgencia::DESCRICAO[$chamado->urgencia] }}</td>
-                        <td style="width: 1%" class="text-nowrap">{{ $chamado->created_at ? $chamado->created_at->format('d/m/Y H:m') : '' }}</td>
-                        <td style="width: 1%" class="text-nowrap">{{ $chamado->updated_at ? $chamado->updated_at->format('d/m/Y H:m') : '' }}</td>
-                        <td><a href="{{ auth()->user()->perfil_id == Perfil::USUARIO ? route('chamados.show', $chamado) : route('chamados.edit', $chamado) }}" class="nav-link text-secondary fw-bold">{{ $chamado->titulo }}</a></td>
+                        <th scope="col">ID</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Urgência</th>
+                        <th scope="col">Abertura</th>
+                        <th scope="col">Última atualização</th>
+                        <th scope="col">Título</th>
                     </tr>
-                @empty
-                    <tr>
-                        <td colspan="99">Nenhum chamado foi localizado</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @forelse($chamados as $chamado)
+                        <tr>
+                            <td style="width: 1%" class="border text-nowrap">{{ $chamado->id }}</td>
+                            <td style="width: 1%" class="border text-nowrap">{{ Status::DESCRICAO[$chamado->status] }}</td>
+                            <td style="width: 1%" class="border text-nowrap">{{ Urgencia::DESCRICAO[$chamado->urgencia] }}</td>
+                            <td style="width: 1%" class="border text-nowrap">{{ $chamado->created_at ? $chamado->created_at->format('d/m/Y H:m') : '' }}</td>
+                            <td style="width: 1%" class="border text-nowrap">{{ $chamado->updated_at ? $chamado->updated_at->format('d/m/Y H:m') : '' }}</td>
+                            <td class="border"><a href="{{ auth()->user()->perfil_id == Perfil::USUARIO ? route('chamados.show', $chamado) : route('chamados.edit', $chamado) }}" class="nav-link text-secondary fw-bold">{{ $chamado->titulo }}</a></td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="99">Nenhum chamado foi localizado</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-layout>
