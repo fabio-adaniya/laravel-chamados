68ba6066cf3ef008c74c33961fba91ad585cf283
fabio-adaniya <f1hiroki@outlook.com>
2023-08-15 17:42:34 -0300

    adição de campos na tela de edição do usuário

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -12,26 +12,43 @@
                 </div>
                 <div class="card-body">
                     <div class="mb-3">
-                        <label for="" class="form-label">Nome</label>
-                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $user->name) }}">
+                        <label for="input-name" class="form-label">Nome <span class="text-danger fw-bold">*</span></label>
+                        <input type="text" name="name" id="input-name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $user->name) }}" required autofocus>
                         @error('name')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="mb-3">
-                        <label for="" class="form-label">Ativo</label>
-                        <select name="ativo" class="form-select @error('ativo') is-invalid @enderror" required>
-                            @if(old('ativo', $user->ativo))
-                                <option value="1" selected>Sim</option>
-                                <option value="0">Não</option>
-                            @else
-                                <option value="1">Sim</option>
-                                <option value="0" selected>Não</option>
-                            @endif
+                        <label for="" class="form-label">Usuário <span class="text-danger fw-bold">*</span></label>
+                        <input type="text" name="username" id="input-username" class="form-control @error('username') is-invalid @enderror" value="{{ old('username', $user->username) }}" required>
+                        @error('username')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">E-mail <span class="text-danger fw-bold">*</span></label>
+                        <input type="email" name="email" id="input-email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', $user->email) }}" required>
+                        @error('email')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="select-ativo" class="form-label">Ativo <span class="text-danger fw-bold">*</span></label>
+                        <select name="ativo" id="select-ativo" class="form-select @error('ativo') is-invalid @enderror" required>
+                            <option value="1" @if(old('ativo', $user->ativo) == '1') selected @endif>Sim</option>
+                            <option value="0" @if(old('ativo', $user->ativo) == '0') selected @endif>Não</option>
                         </select>
                         @error('ativo')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="select-perfil_id" class="form-label">Perfil <span class="text-danger fw-bold">*</span></label>
+                        <select name="perfil_id" id="select-perfil_id" class="form-select @error('perfil_id') is-invalid @enderror" required>
+                            <option value="{{ Perfil::ADMINISTRADOR }}" @if(old('perfil_id', $user->perfil_id) == Perfil::ADMINISTRADOR) selected @endif>{{ Perfil::DESCRICAO[Perfil::ADMINISTRADOR] }}</option>
+                            <option value="{{ Perfil::TECNICO }}" @if(old('perfil_id', $user->perfil_id) == Perfil::TECNICO) selected @endif>{{ Perfil::DESCRICAO[Perfil::TECNICO] }}</option>
+                            <option value="{{ Perfil::USUARIO }}" @if(old('perfil_id', $user->perfil_id) == Perfil::USUARIO) selected @endif>{{ Perfil::DESCRICAO[Perfil::USUARIO] }}</option>
+                        </select>
                     </div>
                 </div>
                 <div class="card-footer teal d-flex">
