bb4c98c7f5993e8a47bf9646616a7d4a365ded98
fabio-adaniya <f1hiroki@outlook.com>
2023-08-15 17:39:33 -0300

    melhorias

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,6 +12,7 @@
             {
                 $("#div-username").show();
                 $("#input-username").prop("disabled", false);
+                $("#input-username").focus();
 
                 $("#div-email").hide();
                 $("#input-email").prop("disabled", true);
@@ -23,6 +24,7 @@
 
                 $("#div-email").show();
                 $("#input-email").prop("disabled", false);
+                $("#input-email").focus();
             }
         }
 
@@ -38,56 +40,62 @@
 
 <x-layout>
     <div class="d-flex justify-content-center vh-100 align-items-center">
-        <div class="card" style="width: 500px">
-            <div class="card-body">
-                <form action="{{ route('login.validar') }}" method="POST">
-                    @csrf
-                    <div class="d-flex justify-content-center mb-3">{{ config('app.name') }}</div>
-                    <div class="d-flex justify-content-center fw-bold mb-3">Login</div>
-                    <div class="d-flex justify-content-center">
-                        <div class="d-flex gap-3">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="radio-username" checked/>
-                                <label class="form-check-label" for="radio-username">Usuário</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="radio-email"/>
-                                <label class="form-check-label" for="radio-email">E-mail</label>
+        <div>
+            <div class="card m-3 border-0 bg-transparent">
+                <div class="card-body">
+                    <h5 class="d-flex justify-content-center">{{ config('app.name') }}</h5>
+                </div>
+            </div>
+            <div class="card m-3" style="width: 500px">
+                <div class="card-body">
+                    <form action="{{ route('login.validar') }}" method="POST">
+                        @csrf
+                        <div class="d-flex justify-content-center mb-3">Login</div>
+                        <div class="d-flex justify-content-center mb-3">
+                            <div class="d-flex gap-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="radio-username" checked/>
+                                    <label class="form-check-label" for="radio-username">Usuário</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="radio-email"/>
+                                    <label class="form-check-label" for="radio-email">E-mail</label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="mb-3" id="div-username">
-                        <label for="input-username" class="form-label">Usuário</label>
-                        <input type="text" id="input-username" name="username" class="form-control @error('username') is-invalid @enderror" value="{{ old('username') }}"/>
-                        @error('username')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="mb-3" id="div-email">
-                        <label for="input-email" class="form-label">E-mail</label>
-                        <input type="text" id="input-email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"/>
-                        @error('email')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label for="input-password" class="form-label">Senha</label>
-                        <input type="password" id="input-password" name="password" class="form-control @error('password') is-invalid @enderror" value="{{ old('password') }}"/>
-                        @error('password')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="d-flex">
-                        <button type="submit" class="btn btn-primary ms-auto">
-                            <i class="fa-solid fa-right-to-bracket"></i> Acessar
-                        </button>
-                    </div>
-                    @error('login')
-                        <div class="d-flex justify-content-center">
-                            <span class="text-danger fw-bold">{{ $message }}</span>
+                        <div class="mb-3" id="div-username">
+                            <label for="input-username" class="form-label">Usuário</label>
+                            <input type="text" id="input-username" name="username" class="form-control @error('username') is-invalid @enderror" value="{{ old('username') }}"/>
+                            @error('username')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
-                    @enderror
-                </form>
+                        <div class="mb-3" id="div-email">
+                            <label for="input-email" class="form-label">E-mail</label>
+                            <input type="text" id="input-email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"/>
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="input-password" class="form-label">Senha</label>
+                            <input type="password" id="input-password" name="password" class="form-control @error('password') is-invalid @enderror" value="{{ old('password') }}"/>
+                            @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="d-flex">
+                            <button type="submit" class="btn btn-primary btn-sm ms-auto">
+                                <i class="fa-solid fa-right-to-bracket"></i> Acessar
+                            </button>
+                        </div>
+                        @error('login')
+                            <div class="d-flex justify-content-center">
+                                <span class="text-danger fw-bold">{{ $message }}</span>
+                            </div>
+                        @enderror
+                    </form>
+                </div>
             </div>
         </div>
     </div>
